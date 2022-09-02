eb2a7d94e71aeb16cf24102778f4f2705e754910
GunchaPy <guntars.reiss@gmail.com>
2022-09-02 13:59:07 +0300

    Day 64: Lesson one. Add Coordinator and manage communication between UIKit and SwiftUI. Display changed image in main view.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -10,7 +10,7 @@ import PhotosUI
 import SwiftUI
 
 // MARK: - Day 64
-
+// Additional work done in Day 63: Lesson Two
 
 // MARK: - Day 63
 // MARK: - Lesson One
@@ -67,6 +67,7 @@ struct LessonOne: View {
 // How to wrap UIViewController in a SwiftUI view
 struct LessonTwoView: View {
     @State private var image: Image?
+    @State private var inputImage: UIImage?
     @State private var showingImagePicker = false
     
     var body: some View {
@@ -78,26 +79,75 @@ struct LessonTwoView: View {
             Button("Select Image") {
                 showingImagePicker = true
             }
+            Button("Remove Image") {
+                image = Image("")
+            }
         }
         .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+            ImagePicker(image: $inputImage)
         }
+        // When new image is chosen, call loadImage()
+        .onChange(of: inputImage) { _ in
+            loadImage()
+        }
+    }
+    
+    // Method to run when property changes
+    func loadImage() {
+        // Check whether inputImage has a value
+        guard let inputImage = inputImage else { return }
+        // Assign image
+        image = Image(uiImage: inputImage)
     }
 }
 
 struct ImagePicker: UIViewControllerRepresentable { // makeUIVC + updateUIVC to conform to protocol
-
+    // Binding so whatever creates this view have binding to image
+    @Binding var image: UIImage?
+    
+    // Create Coordinator class
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        // Tell Coordinator what its parent is
+        var parent: ImagePicker
+        
+        // Modify values directly
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        // Read the response from our PHPicerViewController
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            // Step 1: Tell the picker to go away
+            picker.dismiss(animated: true)
+            
+            // Step 2: Exit if no selection was made
+            guard let provider = results.first?.itemProvider else { return }
+            
+            // Step 3: If there is an image we can use, use it
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+    }
     func makeUIViewController(context: Context) -> some UIViewController {
         // Step 1: Make configuration, asking to provide us only images
         var config = PHPickerConfiguration()
         config.filter = .images
         // Step 2: Create and return the picker that does the actual work of selecting an image
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator // added
         return picker
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         // not used
+    }
+    
+    // Make Coordinator and pass ImagePicker into it
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
 
